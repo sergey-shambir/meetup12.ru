8b1a1fe4017e9b74cb39a1f6103ce7a6e180bc3c
Sergey Shambir <sshambir@yandex.ru>
2019-01-27 10:05:19 +0300

    Упростил заапись тестов БД

--- test/db/test_repository.js
+++ test/db/test_repository.js
@@ -5,7 +5,8 @@ const {assert} = require('chai');
 /**
  * @returns {db.Client}
  */
-async function connect() {
+async function connect()
+{
     const client = new db.Client('postgres://meetup12ru:1234@localhost:15432/meetup12ru');
     await client.connect({quiet: true});
     return client;
@@ -28,113 +29,162 @@ function findById(values, id)
     return value;
 }
 
-describe('meetup', () => {
-    it('can be stored and then be found', async () => {
-        const c = await connect();
-        try
-        {
-            const r = await c.repository();
-            const expected = new Meetup({
-                id: generateId(),
-                createdAt: new Date("2015-03-25"),
-                serviceId: ServiceTimepad,
-                eventId: generateId(),
-                title: "Meetup Title",
-                description: "This is a meetup",
-                startDate: new Date("2015-04-25"),
-                address: "ul. Kirova, 2018"
-            });
-            await r.storeMeetup(expected);
-            try
-            {
-                const meetups = await r.findMeetups();
-                const actual = findById(meetups, expected.id);
-                assert.deepEqual(expected, actual);
-            }
-            finally
-            {
-                await r.deleteMeetup(expected.id);
-            }
-        }
-        finally
-        {
-            c.end();
-        }
-    });
+class DeferContext
+{
+    constructor()
+    {
+        this.deferred = [];
+    }
 
-    it('can be updated', async () => {
+    defer(cb)
+    {
+        this.deferred.push(cb);
+    }
+
+    async end()
+    {
+        for (let cb of this.deferred)
+        {
+            await cb();
+        }
+    }
+}
+
+function wrap(cb)
+{
+    return async () => {
+        const dc = new DeferContext();
         const c = await connect();
         try
         {
             const r = c.repository();
-            const ids = [];
-
-            // Add garbage meetup
-            {
-                let id = generateId();
-                await r.storeMeetup(new Meetup({
-                    id: id,
-                    createdAt: new Date("2011-03-25"),
-                    serviceId: ServiceVK,
-                    eventId: generateId(),
-                    title: "Should never be found",
-                    description: "Should never be found",
-                    startDate: new Date("2011-04-25"),
-                    address: "ul. Pushkina"
-                }));
-                ids.push(id);
-            }
-
-            const expected = new Meetup({
-                id: generateId(),
-                createdAt: new Date("2015-03-25"),
-                serviceId: ServiceTimepad,
-                eventId: generateId(),
-                title: "Meetup Title",
-                description: "This is a meetup",
-                startDate: new Date("2015-04-25"),
-                address: "ul. Kirova, 2018"
-            });
-            await r.storeMeetup(expected);
-            ids.push(expected.id);
-
             try
             {
-                expected.title = "New Meetup Title";
-                await r.storeMeetup(expected);
-                expected.createdAt = new Date("2015-03-30");
-                expected.eventId = "1248412";
-                expected.serviceId = ServiceVK;
-                expected.description = "New Description";
-                expected.startDate = new Date("2015-04-21");
-                expected.address = "nowhere";
-                await r.storeMeetup(expected);
-                const meetups = await r.findMeetups();
-                const actual = findById(meetups, expected.id);
-                assert.deepEqual(expected, actual);
+                await cb(r, dc);
             }
             finally
             {
-                for (let id of ids)
-                {
-                    await r.deleteMeetup(id);
-                }
+                await dc.end();
             }
         }
         finally
         {
             c.end();
         }
-    });
+    };
+}
+
+describe('meetup', () => {
+    it('can be stored and then be found', wrap(async (r, dc) => {
+        const expected = new Meetup({
+            id: generateId(),
+            createdAt: new Date("2015-03-25"),
+            serviceId: ServiceTimepad,
+            eventId: generateId(),
+            title: "Meetup Title",
+            description: "This is a meetup",
+            startDate: new Date("2015-04-25"),
+            address: "ul. Kirova, 2018"
+        });
+        await r.storeMeetup(expected);
+        dc.defer(() => r.deleteMeetup(expected.id));
+
+        const meetups = await r.findMeetups();
+        const actual = findById(meetups, expected.id);
+        assert.deepEqual(expected, actual);
+    }));
+
+    it('can be updated', wrap(async (r, dc) => {
+        // Add one extra meetup to test if find does correct search.
+        {
+            let id = generateId();
+            await r.storeMeetup(new Meetup({
+                id: id,
+                createdAt: new Date("2011-03-25"),
+                serviceId: ServiceVK,
+                eventId: generateId(),
+                title: "Should never be found",
+                description: "Should never be found",
+                startDate: new Date("2011-04-25"),
+                address: "ul. Pushkina"
+            }));
+            dc.defer(() => r.deleteMeetup(id));
+        }
+
+        const expected = new Meetup({
+            id: generateId(),
+            createdAt: new Date("2015-03-25"),
+            serviceId: ServiceTimepad,
+            eventId: generateId(),
+            title: "Meetup Title",
+            description: "This is a meetup",
+            startDate: new Date("2015-04-25"),
+            address: "ul. Kirova, 2018"
+        });
+        await r.storeMeetup(expected);
+        dc.defer(() => r.deleteMeetup(expected.id));
+
+        expected.title = "New Meetup Title";
+        await r.storeMeetup(expected);
+        expected.createdAt = new Date("2015-03-30");
+        expected.eventId = "1248412";
+        expected.serviceId = ServiceVK;
+        expected.description = "New Description";
+        expected.startDate = new Date("2015-04-21");
+        expected.address = "nowhere";
+        await r.storeMeetup(expected);
+        const meetups = await r.findMeetups();
+        const actual = findById(meetups, expected.id);
+        assert.deepEqual(expected, actual);
+    }));
 });
 
 describe('auth', () => {
+    it('can be stored and then be found', wrap(async (r, dc) => {
+        const expected = new Auth({
+            id: generateId(),
+            createdAt: new Date("2018-03-25"),
+            serviceId: ServiceVK,
+            profileId: "94712472147",
+            name: "Vasyan",
+            photoUrl: "http://vk.com/photo124824742312",
+        });
+        await r.storeAuth(expected);
+        dc.defer(() => r.deleteAuth(expected.id));
+        const actual = await r.findAuth(ServiceVK, "94712472147");
+        assert.deepEqual(expected, actual);
+    }));
+
+    it('can be updated', wrap(async (r, dc) => {
+        const expected = new Auth({
+            id: generateId(),
+            createdAt: new Date("2018-03-25"),
+            serviceId: ServiceVK,
+            profileId: "94712472147",
+            name: "Vasyan",
+            photoUrl: "http://vk.com/photo124824742312",
+        });
+        await r.storeAuth(expected);
+        dc.defer(() => r.deleteAuth(expected.id));
+
+        expected.createdAt = new Date("2017-03-25");
+        expected.name = "Petr";
+        expected.photoUrl = "http:/example.com/pic.png";
+        await r.storeAuth(expected);
+
+        const actual = await r.findAuth(ServiceVK, "94712472147");
+        assert.deepEqual(expected, actual);
+    }));
+});
+
+/*
+describe('user', () => {
     it('can be stored and then be found', async () => {
         const c = await connect();
         try
         {
             const r = await c.repository();
-            const expected = new Auth({
+            const auth = new Auth({
                 id: generateId(),
                 createdAt: new Date("2018-03-25"),
                 serviceId: ServiceVK,
@@ -142,45 +192,18 @@ describe('auth', () => {
                 name: "Vasyan",
                 photoUrl: "http://vk.com/photo124824742312",
             });
-            await r.storeAuth(expected);
-            try
-            {
-                const actual = await r.findAuth(ServiceVK, "94712472147");
-                assert.deepEqual(expected, actual);
-            }
-            finally
-            {
-                await r.deleteAuth(expected.id);
-            }
-        }
-        finally
-        {
-            c.end();
-        }
-    });
+            await r.storeAuth(auth);
 
-    it('can be updated', async () => {
-        const c = await connect();
-        try
-        {
-            const r = await c.repository();
-            const expected = new Auth({
+            const expected = new User({
                 id: generateId(),
-                createdAt: new Date("2018-03-25"),
-                serviceId: ServiceVK,
-                profileId: "94712472147",
-                name: "Vasyan",
-                photoUrl: "http://vk.com/photo124824742312",
+                createdAt: new Date("2017-01-25"),
+                primaryAuthId: auth.id,
             });
-            await r.storeAuth(expected);
+            await r.storeUser(expected);
+
             try
             {
-                expected.createdAt = new Date("2017-03-25");
-                expected.name = "Petr";
-                expected.photoUrl = "http:/example.com/pic.png";
-                await r.storeAuth(expected);
-
-                const actual = await r.findAuth(ServiceVK, "94712472147");
+                const actual = await r.findUserWithAuth();
                 assert.deepEqual(expected, actual);
             }
             finally
@@ -194,3 +217,4 @@ describe('auth', () => {
         }
     });
 });
+*/
